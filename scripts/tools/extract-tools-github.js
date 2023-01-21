b002cea342a35f0c8a16d4aa63441b9599aec278
akshatnema <20bcs022@iiitdmj.ac.in>
2023-01-21 23:53:27 +0530

    added request

--- scripts/tools/extract-tools-github.js
+++ scripts/tools/extract-tools-github.js
@@ -1,17 +1,24 @@
 const axios = require('axios');
+const {request} = require('@octokit/request')
 
 const getData = async () => {
   try {
-    const result = await axios.get(
-      'https://api.github.com/search/code?q=filename:.asyncapi-tool',
-      {
-        headers: {
-          accept: 'application/vnd.github.text-match+json',
-          authorization: `token ${process.env.GITHUB_TOKEN}`,
-        },
-      }
-    );
-    console.log(result.data)
+    // const result = await axios.get(
+    //   'https://api.github.com/search/code?q=filename:.asyncapi-tool',
+    //   {
+    //     headers: {
+    //       accept: 'application/vnd.github.text-match+json',
+    //       authorization: `token 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`,
+    //     },
+    //   }
+    // );
+    const result = await request("GET /search/code?q=filename:{query}", {
+      headers: {
+        authorization: `token ${process.env.GITHUB_TOKEN}`,
+      },
+      query: ".asyncapi-tool"
+    });
+    console.log(result)
     return result.data;
   } catch (err) {
     console.log(err);
